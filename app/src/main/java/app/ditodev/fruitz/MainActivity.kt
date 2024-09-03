a9c9c9afb64e5bc324fb4eb38a5ed0890de31594
Gorthdzx <krislaksana123@gmail.com>
2024-09-03 21:30:24 +0700

    feat : onclick listener event with function

--- app/src/main/java/app/ditodev/fruitz/MainActivity.kt
+++ app/src/main/java/app/ditodev/fruitz/MainActivity.kt
@@ -1,13 +1,26 @@
 package app.ditodev.fruitz
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        binding = findViewById(R.id.button)
     }
+
+    fun onNextPage(view :View) {
+        binding.setOnClickListener {
+            Intent(this, NextFruit::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+
 }
