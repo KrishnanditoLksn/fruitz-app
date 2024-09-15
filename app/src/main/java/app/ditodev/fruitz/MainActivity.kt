188ec4170ed360d9e3802c3edcdb85430cd1356b
Gorthdzx <krislaksana123@gmail.com>
2024-09-15 22:16:07 +0700

    refactor : intent to NextFruit

--- app/src/main/java/app/ditodev/fruitz/MainActivity.kt
+++ app/src/main/java/app/ditodev/fruitz/MainActivity.kt
@@ -3,23 +3,24 @@ package app.ditodev.fruitz
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: View
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+    private lateinit var nextBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        binding = findViewById(R.id.button)
+        nextBtn = findViewById(R.id.button)
+        nextBtn.setOnClickListener(this)
     }
 
-    fun onNextPage(view :View) {
-        binding.setOnClickListener {
-            Intent(this, NextFruit::class.java).also {
-                startActivity(it)
-            }
+    override fun onClick(p0: View?) {
+        if (p0?.id != null){
+            val intents = Intent(this@MainActivity , NextFruit::class.java)
+            startActivity(intents)
         }
     }
 
