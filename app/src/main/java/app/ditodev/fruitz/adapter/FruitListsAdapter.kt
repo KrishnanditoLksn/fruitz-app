3cb0841aff70ab19683f73fc4cbc4e825fc055a5
Gorthdzx <krislaksana123@gmail.com>
2024-09-22 20:09:07 +0700

    feat : adapter callback

--- app/src/main/java/app/ditodev/fruitz/adapter/FruitListsAdapter.kt
+++ app/src/main/java/app/ditodev/fruitz/adapter/FruitListsAdapter.kt
@@ -5,14 +5,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import app.ditodev.fruitz.R
 import app.ditodev.fruitz.data.Fruit
 
-class FruitListsAdapter(private  val listFruits : ArrayList<Fruit>) : RecyclerView.Adapter<FruitListsAdapter.ListViewHolder>() {
+class FruitListsAdapter(private val listFruits: ArrayList<Fruit>) :
+    RecyclerView.Adapter<FruitListsAdapter.ListViewHolder>() {
+    private lateinit var onItemClickedCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickedCallback = onItemClickCallback
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.row_fruits , parent , false)
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.row_fruits, parent, false)
         return ListViewHolder(view)
     }
 
@@ -21,19 +28,23 @@ class FruitListsAdapter(private  val listFruits : ArrayList<Fruit>) : RecyclerVi
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        val (name , description , photo) = listFruits[position]
+        val (name, description, photo) = listFruits[position]
         holder.imagePhotos.setImageResource(photo)
         holder.fruitsName.text = name
         holder.fruitsDesc.text = description
 
-        holder.itemView.setOnClickListener{
-            Toast.makeText(holder.itemView.context , "You choose " + listFruits[holder.adapterPosition].name , Toast.LENGTH_SHORT).show()
+        holder.itemView.setOnClickListener {
+            onItemClickedCallback.onItemClicked(listFruits[holder.adapterPosition])
         }
     }
 
-    class ListViewHolder(itemView : View)  : RecyclerView.ViewHolder(itemView) {
-        val imagePhotos  : ImageView = itemView.findViewById(R.id.img_item_photo)
-        val fruitsName : TextView = itemView.findViewById(R.id.tv_item_name)
-        val fruitsDesc : TextView = itemView.findViewById(R.id.tv_item_description)
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imagePhotos: ImageView = itemView.findViewById(R.id.img_item_photo)
+        val fruitsName: TextView = itemView.findViewById(R.id.tv_item_name)
+        val fruitsDesc: TextView = itemView.findViewById(R.id.tv_item_description)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Fruit)
     }
 }
