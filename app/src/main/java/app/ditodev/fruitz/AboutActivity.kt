1012a41259d3b5103313aa8a8b431f083d40f959
Gorthdzx <krislaksana123@gmail.com>
2024-09-22 11:02:55 +0700

    feat : about activity screen

---
+++ app/src/main/java/app/ditodev/fruitz/AboutActivity.kt
@@ -0,0 +1,26 @@
+package app.ditodev.fruitz
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import app.ditodev.fruitz.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityAboutBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.rgb(255,99 , 71 )))
+        supportActionBar!!.setTitle(R.string.about)
+    }
+}
