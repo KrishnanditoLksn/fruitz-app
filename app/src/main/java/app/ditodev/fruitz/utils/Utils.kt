5b208eae135d6bf2a364da160c8ba09a912ce76e
Gorthdzx <krislaksana123@gmail.com>
2024-09-23 14:57:06 +0700

    refactor : moved into utils package

--- app/src/main/java/app/ditodev/fruitz/utils/Utils.kt
+++ app/src/main/java/app/ditodev/fruitz/utils/Utils.kt
@@ -1,10 +1,11 @@
-package app.ditodev.fruitz
+package app.ditodev.fruitz.utils
 
 import android.view.Window
 import android.view.WindowManager
 
 class Utils {
     companion object{
+        const  val EXTRA_DATA = "extra_data"
         fun changeStatusBarColor(window : Window , color: String) {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             window.statusBarColor = android.graphics.Color.parseColor(color)
