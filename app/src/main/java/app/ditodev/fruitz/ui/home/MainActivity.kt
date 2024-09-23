b7e40b2cb44a91ad324a26e69b87171d7863ce63
Gorthdzx <krislaksana123@gmail.com>
2024-09-23 19:11:20 +0700

    refactor : moved to home package

--- app/src/main/java/app/ditodev/fruitz/ui/home/MainActivity.kt
+++ app/src/main/java/app/ditodev/fruitz/ui/home/MainActivity.kt
@@ -1,4 +1,4 @@
-package app.ditodev.fruitz
+package app.ditodev.fruitz.ui.home
 
 import android.content.Intent
 import android.graphics.Color
@@ -7,7 +7,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import app.ditodev.fruitz.R
 import app.ditodev.fruitz.databinding.ActivityMainBinding
+import app.ditodev.fruitz.ui.list.FruitsRecyclerview
+import app.ditodev.fruitz.utils.Utils
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mainBinding: ActivityMainBinding
@@ -17,7 +20,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-        mainBinding.button.setOnClickListener(this)
         mainBinding.btnLists.setOnClickListener(this)
         Utils.changeStatusBarColor(window, "#3498db")
 
@@ -29,11 +31,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(p0: View?) {
         if (p0?.id != null) {
             when (p0.id) {
-                R.id.button -> {
-                    val intents = Intent(this@MainActivity, NextFruit::class.java)
-                    startActivity(intents)
-                }
-
                 R.id.btnLists -> {
                     val btnLists = Intent(this@MainActivity, FruitsRecyclerview::class.java)
                     startActivity(btnLists)
